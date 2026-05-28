a6b0b69c507832ebfb172a8039eea4744a0c39a0
mutdakrushi-png <mutdakrushi@gmail.com>
2026-05-28 10:31:32 +0530

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -5,12 +5,11 @@
 
 def seconds_in_week():
 
+  return(return)
+  print(seconds_in_week)
+  a=60
+  b=60*a
+  c=24*b
+  d=7*6
+  result=d
   return(result)
-
-print(seconds_in_week)
-a=60
-b=60*a
-c=24*b
-d=7*6
-result=d
-return(result)
