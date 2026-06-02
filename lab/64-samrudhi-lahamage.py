04e1e49516375ecba5212bbf60c5c7c704a0247b
samrudhilahamage-ship-it <samrudhilahamage@gmail.com>
2026-06-02 11:32:54 +0530

    Correct syntax and implement even/odd checker
    Fix syntax errors and add number classification function.

--- lab/64-samrudhi-lahamage.py
+++ lab/64-samrudhi-lahamage.py
@@ -2,3 +2,15 @@ x=0
 while x<10
     x=x+1
 print(x)
+# problem 2
+a=4
+b=9
+c=19
+def number(n):
+    if n % 2==0:
+      print("even")
+    else:
+        print("odd")
+number(a)
+number(b)
+number(c)
