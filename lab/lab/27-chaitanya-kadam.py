26168b63c6a1d15a4ffbb7f1fa3dea8cf80cac4e
chaitanyakadam7774-ops <chaitanyakadam7774@gmail.com>
2026-06-02 11:47:28 +0530

    Update 27-chaitanya-kadam.py

--- lab/lab/27-chaitanya-kadam.py
+++ lab/lab/27-chaitanya-kadam.py
@@ -17,3 +17,18 @@ c=19
 print("a",even_odd(a))
 print("b",even_odd(b))
 print("c",even_odd(c))
+
+#problem 3
+
+def div_3(num):
+    if num %3==0:
+        return("divisible by 3")
+    else:
+        return("not divisible by 3")
+a=4
+b=9
+c=19
+print("a",div_3(a))
+print("b",div_3(b))
+print("C",div_3(C))
+        
