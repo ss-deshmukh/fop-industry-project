8b2678c9b5dd61255e617235f5c4b353459de756
chaitanyakadam7774-ops <chaitanyakadam7774@gmail.com>
2026-06-02 11:35:12 +0530

    Update 27-chaitanya-kadam.py

--- lab/lab/27-chaitanya-kadam.py
+++ lab/lab/27-chaitanya-kadam.py
@@ -4,3 +4,16 @@ while x<10:
     x=x+1
     total=total+x
  print(total)
+
+#problem 2
+def even_odd(number):
+    if number%2==0:
+        return "even"
+    else:
+        return "odd"
+a=4
+b=9
+c=19
+print("a",even_odd(a))
+print("b",even_odd(b))
+print("c",even_odd(c))
