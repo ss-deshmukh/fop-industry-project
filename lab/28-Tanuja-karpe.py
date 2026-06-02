ee75d502f7b54b4a714cd327c86f39214f9d85bd
tanukarpe03-create <tanukarpe03@gmail.com>
2026-06-02 11:44:55 +0530

    Update 28-Tanuja-karpe.py

--- lab/28-Tanuja-karpe.py
+++ lab/28-Tanuja-karpe.py
@@ -10,3 +10,18 @@ else:
 even_or_odd(a)
 even_or_odd(b)
 even_or_odd(c)
+
+#problem3
+a=4
+b=9
+c=19
+def even_or_odd(n):
+if(n%3=0):
+  print("even")
+else:
+  print("odd")
+  
+even_or_odd(a)
+even_or_odd(b)
+even_or_odd(c)
+
