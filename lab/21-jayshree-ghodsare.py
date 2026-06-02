45f56004e5643077ebef1b0285198d32043a373b
jayshreeghodsare-coder <jayshreeghodsare@gmail.com>
2026-06-02 12:00:22 +0530

    Update 21-jayshree-ghodsare.py

--- lab/21-jayshree-ghodsare.py
+++ lab/21-jayshree-ghodsare.py
@@ -3,7 +3,6 @@ while x<10:
 x=x+1
 print(x)
 #problem2 
-
 a=2
 b=26
 c=37
@@ -13,3 +12,13 @@ for x in l:
        print(f"Even")
      else:
        print(f"odd")
+#problem3
+a=4
+b=9
+c=19
+m=[a,b,c]
+for x in m:
+     if x % 3 ==0:
+          print("divis")
+     else:
+          print("divisible")
