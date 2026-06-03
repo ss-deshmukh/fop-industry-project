5e0e74c93a1e3b3397ed9a8a8e8931418549fc50
chaitanyakadam7774-ops <chaitanyakadam7774@gmail.com>
2026-06-03 11:35:34 +0530

    Update 27-chaitanya-kadam.py

--- lab/lab/27-chaitanya-kadam.py
+++ lab/lab/27-chaitanya-kadam.py
@@ -42,3 +42,20 @@ def print_ele(a):
 
 print_ele(a)
 
+#problem 5
+l=[1,2,3,4,5,6]
+l.pop(0)
+print(l)
+l.pop(1)
+print(l)
+l.pop(2)
+print(l)
+
+#problem 6
+l=[1,2,3,4,5,6]
+l.pop(4)
+print(l)
+l.pop(2)
+print(l)
+l.pop(0)
+print(l)
