b4e551b00cb027d696cee205038ae84ec84310eb
dhumalom2008-ship-it <dhumalom2008@gmail.com>
2026-06-04 11:27:28 +0530

    Update 17-om-dhumal.py

--- lab/17-om-dhumal.py
+++ lab/17-om-dhumal.py
@@ -1,20 +1,20 @@
 x=0
 total=0
 while x<10:
-X=X+1
-total total+x
-print(total)
+ X=X+1
+ total=total+1
+ print(total)
 
 #problem 2
 
 def even_odd(number):
 if number%2==0:
-return "even"
+   return "even"
 else:
-return "odd"
+   return "odd"
 a-4
 b-9
-<-19
+c-19
 print("a", even_odd(a))
 print("b",even_odd(b))
 print("c",even_odd(c))
@@ -22,15 +22,15 @@ print("c",even_odd(c))
 
 def div_3(num):
 if num %3==0:
-return("divisible by 3")
+    return("divisible by 3")
 else:
-return("not divisible by 3")
+    return("not divisible by 3")
 a=4
 b=9
-C=19
+c=19
 print("a", div_3(a))
 print("b", div_3(b))
-print("C", div_3(C))
+print("c", div_3(c))
 
 
 
@@ -41,6 +41,7 @@ def print_ele(a):
   for x in a:
     i=i+1
     print(1)
+    
 print_ele(a)
 
 # problem 5
