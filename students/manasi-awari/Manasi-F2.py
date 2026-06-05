3cdf4ffda0edf9bd7b1dc5cf1faf434c6ddec601
manasi20061122 <mansiawari2006@gmail.com>
2026-06-05 11:39:30 +0530

    Update Manasi-F2.py

--- students/manasi-awari/Manasi-F2.py
+++ students/manasi-awari/Manasi-F2.py
@@ -29,3 +29,17 @@ for x in s:
         continue
 
 print("Number of vowels =", c)
+ #problem 10
+s = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+     'h', 'i', 'j', 'k', 'l', 'm', 'n',
+     'o', 'p', 'q', 'r', 's', 't', 'u',
+     'v', 'w', 'x', 'y', 'z'}
+
+c = 0
+
+for x in s:
+    if x != 'a' and x != 'e' and x != 'i' and x != 'o' and x != 'u':
+        c = c + 1
+
+print("Number of consonants =", c)
+
