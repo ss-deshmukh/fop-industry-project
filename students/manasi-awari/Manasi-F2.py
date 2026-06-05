040bd5e648526440641f905984f001639b4d9bca
manasi20061122 <mansiawari2006@gmail.com>
2026-06-05 11:29:45 +0530

    Add more letters to the set in Manasi-F2.py

--- students/manasi-awari/Manasi-F2.py
+++ students/manasi-awari/Manasi-F2.py
@@ -1,1 +1,8 @@
+#problem 8
+s = {'a', 'b', 'c', 'd'}
 
+s.update({'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+          'w', 'x', 'y', 'z'})
+
+print(s)
