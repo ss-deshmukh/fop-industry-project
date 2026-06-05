39491dcf0ea7c5f547f46f9004d0fd33b51798db
dawangesakshi94-hue <dawangesakshi94@gmail.com>
2026-06-05 11:37:44 +0530

    Update 9-sakshi-dawange.py

--- students/sakshi-dawange/9-sakshi-dawange.py
+++ students/sakshi-dawange/9-sakshi-dawange.py
@@ -1,1 +1,15 @@
+problem1
+d={"ram":30,"vijay":40,"radha":60}
+print(d["vijay"])
 
+problem2
+d={"ram":30,"vijay":40,"radha":60}
+d.update({"tom":2,"don":10})
+print(d)
+
+problem3
+s={'a','e','i','o','u'}
+c=0
+v={'a','e','i','o','u'}
+for x in v:
+  
