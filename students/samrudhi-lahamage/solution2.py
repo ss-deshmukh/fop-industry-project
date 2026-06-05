a1139ed8fecd7a7ddf55bba643a61cffcdbf431a
samrudhilahamage-ship-it <samrudhilahamage@gmail.com>
2026-06-05 12:06:04 +0530

    Update solution2.py

--- students/samrudhi-lahamage/solution2.py
+++ students/samrudhi-lahamage/solution2.py
@@ -4,3 +4,15 @@ for x in s:
     if x not in ['a','e','i','o','u']:
       count=count+1
 print(count)
+#problem2
+s={'a','b','c','d','e','f','g','h','i','j','k','l','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+vowels=set()
+consonents=set()
+for x in s:
+    if x  in['a','e','i','o','u']:
+        vowels.add(x)
+    else:
+        consonents.add(x)
+print("vowels=",vowels)
+print("consonents=",consonents)
+
