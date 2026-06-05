6082ccad6d17584eef9f33c4963033f2b322be29
manasi20061122 <mansiawari2006@gmail.com>
2026-06-05 09:29:42 +0530

    Update 53-manasi-awari.py

--- 53-manasi-awari.py
+++ 53-manasi-awari.py
@@ -43,3 +43,14 @@ d={"Ram": 30,"Vijay": 40,"Radha": 60}
 d.update({"tom" : 10})
 d.update({"don": 2})
 print(d)
+ #problems 8
+s = {'a', 'b', 'c', 'd'}
+
+s.update({'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+
+'w', 'x', 'y', 'z'})
+
+print(s)
+
