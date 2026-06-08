8d870bf010297ff991656c64a5ef31bc863201b7
devshri0826 <devshrigadhave0830@gmail.com>
2026-06-08 12:36:14 +0530

    Delete number guessing program from Devshri-F2.py
    Removed program 5 that prompts for a number guess.

--- students/devshri-gadhave/Devshri-F2.py
+++ students/devshri-gadhave/Devshri-F2.py
@@ -67,15 +67,3 @@ for s in letters:
 print("Vowels Set =", vowels)
 print("Consonants Set =", consonants)
 
-#program 5
-
-num = 10
-
-unum = int(input("Guess a number between 1 to 20: "))
-
-if unum < num:
-    print("Low")
-elif unum > num:
-    print("High")
-else:
-    print("Correct")
