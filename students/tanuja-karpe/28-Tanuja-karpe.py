7184e507e7f4fe1c09e1c5d752b99d8d79dabfbb
tanukarpe03-create <tanukarpe03@gmail.com>
2026-06-09 11:47:23 +0530

    Update 28-Tanuja-karpe.py

--- students/tanuja-karpe/28-Tanuja-karpe.py
+++ students/tanuja-karpe/28-Tanuja-karpe.py
@@ -1,1 +1,44 @@
+t=0
+n=1
+while n<=10:
+  t+=n
+  n+=1
+print(t)
+#problem2
+a=4
+b=9
+c=19
+def check(n):
+     if(n%2=0):
+        print("even")
+     else:
+        print("odd")
 
+check(a)
+check(b)
+check(c)
+#problem3
+a=4
+b=9
+c=19
+def check(n):
+     if(n%3==0):
+      print("divisible")
+     else:
+      print ("not divisible ")
+check(a)
+check(b)
+check(c)
+#problem 4
+n=[1,2,3]
+def check(n=list):
+     for x in n:
+         print (x)
+         
+check (n)
+#problem5
+l=[1,2,3,4,5,6]
+l.pop(0)
+l.pop(1)
+l.pop(2)
+print (l)
