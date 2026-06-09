2bf38c6334eba5e13ce159d9874513f4c91874ce
ishwarimahaddev-cell <ishwarimahaddev@gmail.com>
2026-06-09 11:24:50 +0530

    Update solution3.py

--- students/ishwari-mahaddev/solution3.py
+++ students/ishwari-mahaddev/solution3.py
@@ -1,6 +1,10 @@
 set={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
-c=0
+vowel = []
+consonants = []
 for x in set:
-    if x not in ('a','e','i','o','u'):
-         c=c+1
-print(c)
+    if x not in {'a','e','i','o','u'}:
+         consonants.append(x)
+    else:
+        vowel.append(x)
+print(vowel)
+print(consonants)
