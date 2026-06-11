9cf49b8f28039763136d348811d7db15399dfe8d
manasi20061122 <mansiawari2006@gmail.com>
2026-06-11 11:48:24 +0530

    Update Awari-11.py

--- students/manasi-awari/Awari-11.py
+++ students/manasi-awari/Awari-11.py
@@ -15,3 +15,29 @@ l=float(input("length:"))
 b=float(input("breath:"))
 results=area(l,b)
 print(results)
+
+   #problem 1
+
+x = "name_surname_age"
+
+info = x.split("_")
+
+print(info)
+
+#problem 
+
+def calculate(l, b, h):
+    if (h>0):
+        volume = l*b*h
+        perimeter = 4 * (l+b+h)
+        return ( volume , perimeter)
+    
+    else:
+        area = l*b
+        perimeter = 2 * (l+b)
+        return ( area, perimeter)
+
+
+
+print("Cuboid:", calculate(5, 3, 4))
+print("Rectangle:", calculate(5, 3,0))
