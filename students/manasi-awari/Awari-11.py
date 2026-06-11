005fadc45b960581c86cc26a6d9c6bafd6e8131b
manasi20061122 <mansiawari2006@gmail.com>
2026-06-11 10:06:50 +0530

    Update Awari-11.py

--- students/manasi-awari/Awari-11.py
+++ students/manasi-awari/Awari-11.py
@@ -1,1 +1,17 @@
-
+# problem 
+d={}
+m=int(input("no of members:"))
+members={}
+for x in range(m):
+    name=input("enter members name:")
+    age=int(input("enter members age:")) 
+    d.update({name : age})
+    print (d)
+   # problem 
+def area(l,b):
+    a=l*b
+    return(a)
+l=float(input("length:"))
+b=float(input("breath:"))
+results=area(l,b)
+print(results)
