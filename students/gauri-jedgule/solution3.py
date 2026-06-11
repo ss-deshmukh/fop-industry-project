2a42e33789f8189b37671decf284d515767f384f
jedgulegauri789-ctrl <jedgulegauri789@gmail.com>
2026-06-11 11:45:49 +0530

    Update solution3.py

--- students/gauri-jedgule/solution3.py
+++ students/gauri-jedgule/solution3.py
@@ -5,3 +5,12 @@ for i in range(5):
     name = input("Enter name for student (i + 5): ")
     student.append(name) 
 print("List of students:", student)
+
+
+#title 2
+#find area
+def area(a,b):
+    return a * b
+
+print(area(2,4))
+    
