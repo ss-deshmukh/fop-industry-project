dab1bfe8e5a8f63da9cdf571c9431883b41800c4
dnyaneshwarimalunjkar4-netizen <dnyaneshwarimalunjkar4@gmail.com>
2026-06-11 11:38:16 +0530

    Update Dnyaneshwari-f4.py

--- students/dnyaneshwari-malunjkar/Dnyaneshwari-f4.py
+++ students/dnyaneshwari-malunjkar/Dnyaneshwari-f4.py
@@ -5,3 +5,21 @@ x = "name_surname_age"
 info = x.split("_")
 
 print(info)
+
+#problem 
+
+def calculate(l, b, h):
+    if (h>0):
+        volume = l*b*h
+        perimeter = 4 * (l+b+h)
+        return ( volume , perimeter)
+    
+    else:
+        area = l*b
+        perimeter = 2 * (l+b)
+        return ( area, perimeter)
+
+
+
+print("Cuboid:", calculate(5, 3, 4))
+print("Rectangle:", calculate(5, 3,0))
