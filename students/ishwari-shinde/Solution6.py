d2904e1a46faa1cb94d72699761b501ead1e5aff
shindeishwari642-cyber <shindeishwari642@gmail.com>
2026-06-12 11:42:25 +0530

    Create Solution6.py

---
+++ students/ishwari-shinde/Solution6.py
@@ -0,0 +1,9 @@
+#problem 
+#information
+name = input("Name: ")
+age = int(input("Age: "))
+city = input("City: ")
+lang = input("Favourite language: ")
+years = int(input("Coding years: "))
+
+print(f"Name: {name}\nAge: {age}\nCity: {city}\nLanguage: {lang}\nCoding Years: {years}")
