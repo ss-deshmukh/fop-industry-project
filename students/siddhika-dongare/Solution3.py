6f861b4daf44535158c3726e7244effc5b91c074
siddhikadongare98-code <siddhikadongare98@gmail.com>
2026-06-13 10:58:17 +0530

    Update Solution3.py

--- students/siddhika-dongare/Solution3.py
+++ students/siddhika-dongare/Solution3.py
@@ -25,4 +25,20 @@ year= int(input("enter a year:"))
 is_leap_year(year)
 
 #problem3
+def sum_to_n(n):
+    total = 0  # running total
 
+    for i in range(1, n + 1):
+        total += i
+
+    return total 
+    
+print(sum_to_n(5))   # 15
+print(sum_to_n(1))   # 1
+print(sum_to_n(10))  # 55
+
+#problem4
+def times_table(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+times_table(7)
