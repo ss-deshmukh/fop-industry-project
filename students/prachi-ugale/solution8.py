c0300135b04d349287643aa230d01ca24884b96a
ugaleprachi789-sketch <ugaleprachi789@gmail.com>
2026-06-13 10:54:40 +0530

    Update solution8.py

--- students/prachi-ugale/solution8.py
+++ students/prachi-ugale/solution8.py
@@ -21,3 +21,12 @@ def is_leap_year(year):
         print("False")
 year=int(input("Enter a year="))
 is_leap_year(year)
+
+#problem 3
+def sum_to_n(n):
+    t=0
+    for x in range(1,n+1):
+        t=t+x
+    return t
+number=int(input("Enter a number="))
+print(sum_to_n(number))
