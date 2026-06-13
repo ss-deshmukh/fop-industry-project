56f731d9660356f098845d4f24413722312c1bbe
anjaliyadav32607-create <anjaliyadav32607@gmail.com>
2026-06-13 11:02:44 +0530

    Update Solution7.py

--- students/anjali-yadav/Solution7.py
+++ students/anjali-yadav/Solution7.py
@@ -1,1 +1,16 @@
-
+#problem 1
+def classify_grade(score):
+    if score>=90:
+        print("A")
+    elif score>=80:
+        print("B")
+    elif score>=70:
+        print("C")
+    elif score>=60:
+        print("D")
+    elif score<35:
+        print("F")
+    else:
+        print("pass")
+score=int(input("Enter score:"))   
+classify_grade(score)
