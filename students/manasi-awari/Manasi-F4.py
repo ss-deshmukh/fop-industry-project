6200de8fd2e3a5e6e18407b5c5f956172b8bcce8
manasi20061122 <mansiawari2006@gmail.com>
2026-06-13 10:15:12 +0530

    Update Manasi-F4.py

--- students/manasi-awari/Manasi-F4.py
+++ students/manasi-awari/Manasi-F4.py
@@ -1,1 +1,17 @@
+def classify_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
 
+print(classify_grade(95))
+print(classify_grade(85))
+print(classify_grade(75))
+print(classify_grade(65))
+print(classify_grade(45))
