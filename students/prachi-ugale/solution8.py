a5276b62ba18d88bdd0110ffd4ff4dc466aa701d
ugaleprachi789-sketch <ugaleprachi789@gmail.com>
2026-06-13 10:29:04 +0530

    Update solution8.py

--- students/prachi-ugale/solution8.py
+++ students/prachi-ugale/solution8.py
@@ -1,1 +1,14 @@
-
+#problem 1
+score=int(input("enter score="))
+def classify_grade(score):
+    if score>=90:
+        print("A")
+    elif score>=80 and score<90:
+        print("B")
+    elif score>=70 and score<80:
+        print("C")
+    elif score>=60 and score<70:
+        print("D")
+    else: 
+        print("F")
+classify_grade(score)
