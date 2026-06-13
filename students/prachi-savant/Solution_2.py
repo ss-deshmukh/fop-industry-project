f05e25c04e4c87f6a8d4e28aaf70dde90dfe6101
sawantd581-wq <sawantd581@gmail.com>
2026-06-13 10:59:23 +0530

    Update Solution_2.py

--- students/prachi-savant/Solution_2.py
+++ students/prachi-savant/Solution_2.py
@@ -40,4 +40,11 @@ def sum_to_n(n):
 print(sum_to_n(5))   # 15
 print(sum_to_n(1))   # 1
 print(sum_to_n(10))  # 55
+
+#problem 5
+def table(n)
+for i in range(1,11):
+    print(n*i)
+    table(7)
+
     
