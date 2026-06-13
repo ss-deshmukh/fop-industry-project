16b3b0c35602b7494af9ec7b6862cba3aaadd27a
sawantd581-wq <sawantd581@gmail.com>
2026-06-13 10:16:52 +0530

    Update Solution_2.py

--- students/prachi-savant/Solution_2.py
+++ students/prachi-savant/Solution_2.py
@@ -2,3 +2,15 @@ def area(l, b):
     return l * b
 
 print(area(5, 4))
+#problem 2
+def classify_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
