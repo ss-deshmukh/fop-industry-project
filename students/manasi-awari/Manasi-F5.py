5d039dfee2284ade3511796b38558b1fd6352431
manasi20061122 <mansiawari2006@gmail.com>
2026-06-15 11:14:30 +0530

    Update Manasi-F5.py

--- students/manasi-awari/Manasi-F5.py
+++ students/manasi-awari/Manasi-F5.py
@@ -1,3 +1,13 @@
+#problem
+def countdown(n):
+    while n > 0:
+        print(n)
+        n = n-1
+    print("Liftoff!")
+
+countdown(5)
+
+ #problem 
 def print_triangle(height):
     for row in range(1, height + 1):
         print('*' * row)
