fd3293aaa8af5895486668611985ec5ca715a628
shreyasatishdeshmukh803-droid <shreyasatishdeshmukh803@gmail.com>
2026-06-15 11:36:36 +0530

    Update Solution 7.py

--- students/shreya-deshmukh/Solution 7.py
+++ students/shreya-deshmukh/Solution 7.py
@@ -16,3 +16,23 @@ def print_triangle(height):
         print("*"*row)
         row=row+1
 print_triangle(10)
+
+#problem 3
+SECRET = 10
+
+def play_game():
+    guesses = 0
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        guesses =+1
+
+        if guess > SECRET:
+            print("Too high!")
+        elif guess < SECRET:
+            print("Too low!")
+        else:
+            print(f"Correct! You got it in {guesses} tries.")
+            break
+
+play_game()
