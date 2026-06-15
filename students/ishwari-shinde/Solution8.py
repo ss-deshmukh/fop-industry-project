115a29c66be7f40a8657ad4507083aead7fd7812
shindeishwari642-cyber <shindeishwari642@gmail.com>
2026-06-15 11:18:19 +0530

    Update Solution8.py

--- students/ishwari-shinde/Solution8.py
+++ students/ishwari-shinde/Solution8.py
@@ -1,4 +1,17 @@
+#problem 1
+#triangle range
 def print_triangle(h):
     for i in range(1, h + 1):
         print("*" * i)
 print_triangle(5)
+
+#problem 2
+#countdown
+def countdown (n):
+    while n >=1:
+        print(n)
+        n-=1
+     print("liftoff!")
+countdown (5)
+
+
