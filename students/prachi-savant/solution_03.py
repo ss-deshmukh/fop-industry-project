14f58f79daac280268d4c235d245aff752fd172c
sawantd581-wq <sawantd581@gmail.com>
2026-06-15 11:15:47 +0530

    Update solution_03.py

--- students/prachi-savant/solution_03.py
+++ students/prachi-savant/solution_03.py
@@ -1,1 +1,31 @@
+#problem1
+def countdown(n):
+    while n >= 1:
+        print(n)
+        n = n - 1
+    print("Liftoff!")
 
+countdown(5)
+
+#problem 2
+def first_multiple(n, start):
+    x = start
+
+    while True:
+        if x % n == 0:
+            break
+        x = x + 1
+
+    return x
+
+
+print(first_multiple(5, 12))
+
+#problem 3
+def print_triangle(height):
+    row = 1
+    while row <= height:
+        print("*" * row)
+        row += 1
+
+print_triangle(5)
