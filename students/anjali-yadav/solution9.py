48b0df0b0408d3d5d9d9122edec539c8500bbe76
anjaliyadav32607-create <anjaliyadav32607@gmail.com>
2026-06-15 11:41:15 +0530

    Update solution9.py

--- students/anjali-yadav/solution9.py
+++ students/anjali-yadav/solution9.py
@@ -1,15 +1,15 @@
 #problem 1
-SECRET=10
+SECRET = 10
 def play_game():
-    guess=0
+    guesses = 0
     while True:
-        guess=int(input("Enter your guess: "))
-        guess=guess+1
-        if guess>SECRET:
-            print("Too high")
-        elif guess<SECRET:
-            print("Too low")
+        guess = int(input("Enter your guess: "))
+        guesses =+1
+        if guess > SECRET:
+            print("Too high!")
+        elif guess < SECRET:
+            print("Too low!")
         else:
-            print("Correct You got it in guesses tries.")
+            print(f"Correct! You got it in {guesses} tries.")
             break
 play_game()
