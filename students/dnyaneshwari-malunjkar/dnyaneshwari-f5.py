e269737801daa7433e0870a7916b5749a62b44cd
dnyaneshwarimalunjkar4-netizen <dnyaneshwarimalunjkar4@gmail.com>
2026-06-15 11:14:28 +0530

    Update dnyaneshwari-f5.py

--- students/dnyaneshwari-malunjkar/dnyaneshwari-f5.py
+++ students/dnyaneshwari-malunjkar/dnyaneshwari-f5.py
@@ -6,3 +6,10 @@ def countdown(n):
     print("Liftoff!")
 
 countdown(5)
+# problm 2
+def print_triangle(height):
+    for row in range(1, height + 1):
+        print('*' * row)
+
+# Example
+print_triangle(5)
