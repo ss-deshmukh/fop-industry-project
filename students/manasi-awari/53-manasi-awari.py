184586d4c8e791240297aa027273d32664143881
ss-deshmukh <s.s.deshmukh00@gmail.com>
2026-06-16 17:14:33 +0530

    fix: move 53-manasi-awari.py to correct student directory

--- students/manasi-awari/53-manasi-awari.py
+++ students/manasi-awari/53-manasi-awari.py
@@ -43,3 +43,36 @@ d={"Ram": 30,"Vijay": 40,"Radha": 60}
 d.update({"tom" : 10})
 d.update({"don": 2})
 print(d)
+ #problems 8
+s = {'a', 'b', 'c', 'd'}
+
+s.update({'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+
+'w', 'x', 'y', 'z'})
+
+print(s)
+ #problem 9
+s = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+     'h', 'i', 'j', 'k', 'l', 'm', 'n',
+     'o', 'p', 'q', 'r', 's', 't', 'u',
+     'v', 'w', 'x', 'y', 'z'}
+
+c = 0
+
+for x in s:
+    if x == 'a':
+        c = c + 1
+    elif x == 'e':
+        c = c + 1
+    elif x == 'i':
+        c = c + 1
+    elif x == 'o':
+        c = c + 1
+    elif x == 'u':
+        c = c + 1
+    else:
+        continue
+
+print("Number of vowels =", c)
