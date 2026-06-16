8a51b3754e2f8b4810b0286ca7fbca3b89116ca2
ss-deshmukh <s.s.deshmukh00@gmail.com>
2026-06-16 11:34:25 +0530

    fix: wrap in seconds_in_week() function and fix syntax error

--- students/rushi-mutaduk/Sec_in_week.py
+++ students/rushi-mutaduk/Sec_in_week.py
@@ -1,5 +1,9 @@
-s_in_min=60
-s_in_hr=60*s_in_min
-s_in_day=24*s_in_hr
-s_in_week=7*s_in_day
-print("Seconds in week : ",s_in_week
+def seconds_in_week():
+    s_in_min = 60
+    s_in_hr = 60 * s_in_min
+    s_in_day = 24 * s_in_hr
+    s_in_week = 7 * s_in_day
+    return s_in_week
+
+
+print(seconds_in_week())
