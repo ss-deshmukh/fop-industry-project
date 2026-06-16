c11133aa594e6dcfac1955039659e69169b846fd
anjaliyadav32607-create <anjaliyadav32607@gmail.com>
2026-06-16 11:18:53 +0530

    Update solution10.py

--- students/anjali-yadav/solution10.py
+++ students/anjali-yadav/solution10.py
@@ -1,1 +1,14 @@
-
+#problem 1
+def add_item(total, price):
+    return total + price
+def apply_tax(total):
+    return total * 1.05
+def main():
+    total = 0
+    total = add_item(total, 40)
+    total = add_item(total, 25)
+    total = add_item(total, 15)
+    print("Cart total:", total)
+    taxed_total = apply_tax(total)
+    print("With tax:", taxed_total)
+main()
