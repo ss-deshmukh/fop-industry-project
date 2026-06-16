c1475260f4b14ab9c51736c2e32c42f5479c4801
akshadasadgir87-png <akshadasadgir87@gmail.com>
2026-06-16 11:37:43 +0530

    Update solution10.py

--- students/akshada-sadgir/solution10.py
+++ students/akshada-sadgir/solution10.py
@@ -12,3 +12,15 @@ def main():
     taxed_total = apply_tax(total)
     print("With tax:", taxed_total)
 main()
+#problem2
+def circle(r,pi=3.14159):
+    a=pi*r*r
+    circumference=2*pi*r
+    return a,circumference
+a,c=circle(2)
+print(round(a,2))
+print(round(c,2))
+
+a,c=circle(2,pi=3.14)
+print(round(a,2))
+print(round(c,2))
