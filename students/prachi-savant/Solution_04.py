c24b664b03876d5ea5c2e2d1fff1684a1daeb641
sawantd581-wq <sawantd581@gmail.com>
2026-06-16 11:48:49 +0530

    Update Solution_04.py

--- students/prachi-savant/Solution_04.py
+++ students/prachi-savant/Solution_04.py
@@ -45,3 +45,33 @@ def rectangle(width, height):
 a, p = rectangle(5, 3)
 print("Area:", a)
 print("Perimeter:", p)
+
+#problem 4
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+def modulo(a, b):
+    if b == 0:
+        return "Cannot modulo by zero"
+    return a % b
+
+
+a = 10
+b = 5
+
+print("Add:", add(a, b))
+print("Subtract:", subtract(a, b))
+print("Multiply:", multiply(a, b))
+print("Divide:", divide(a, b))
+print("Modulo:", modulo(a, b))
