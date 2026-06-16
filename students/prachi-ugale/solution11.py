79de4e5795c08641c87452d65e80f5b97c4db0f2
ugaleprachi789-sketch <ugaleprachi789@gmail.com>
2026-06-16 11:37:31 +0530

    Update solution11.py

--- students/prachi-ugale/solution11.py
+++ students/prachi-ugale/solution11.py
@@ -12,3 +12,31 @@ def main():
     taxed_total = apply_tax(total)
     print("With tax:", taxed_total)
 main()
+
+#problem 2
+
+def rectangle(width,height):
+    area=width*height
+    perimeter=2*(width+height)
+    return area,perimeter
+a,p=rectangle(3,4)
+print(a)
+print(p)
+
+a,p=rectangle(10,2)
+print(a)
+print(p)
+
+#problem 3
+
+def circle(r,pi=3.14159):
+    a=pi*r*r
+    circumference=2*pi*r
+    return a,circumference
+a,c=circle(2)
+print(round(a,2))
+print(round(c,2))
+
+a,c=circle(2,pi=3.14)
+print(round(a,2))
+print(round(c,2))
