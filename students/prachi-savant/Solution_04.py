2e91aa3f02b8d812515affd3e3018088e5847be7
sawantd581-wq <sawantd581@gmail.com>
2026-06-16 11:21:53 +0530

    Implement shopping cart and user input functions
    Added functions to calculate cart total, tax, and gather user information.

--- students/prachi-savant/Solution_04.py
+++ students/prachi-savant/Solution_04.py
@@ -1,1 +1,47 @@
+#problem 1total = 0
 
+def add_item(price):
+    global total
+    total += price
+    return total
+
+def apply_tax():
+    return total * 1.05
+
+def main():
+    add_item(40)
+    add_item(25)
+    add_item(15)
+
+    print(f"Items added. Cart total: {total}")
+    print(f"With 5% tax: {apply_tax():.2f}")
+
+if __name__ == "__main__":
+    main()
+  
+  #problem 2
+def get_name():
+    return input("Name: ")
+
+def get_city():
+    return input("city: ")
+    
+def get_language():
+    return input("language: ")
+
+def main():
+    name = get_name()
+    city = get_city()
+    language = get_language()
+
+main()
+
+#problem 3
+def rectangle(width, height):
+    area = width * height
+    perimeter = 2 * (width + height)
+    return area, perimeter
+
+a, p = rectangle(5, 3)
+print("Area:", a)
+print("Perimeter:", p)
