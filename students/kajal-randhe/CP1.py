02e711b7a09bebef57d5925f6172f05e57ba16ec
kajalrandhe591-web <kajalrandhe591@gmail.com>
2026-06-17 10:27:55 +0530

    CP1.py

---
+++ students/kajal-randhe/CP1.py
@@ -0,0 +1,29 @@
+pass_mark=35
+distinction_mark=75
+def average_of_three(m1,m2,m3):
+    avg=(m1+m2+m3)/3
+    return round(avg,1)
+def classify_result(avg):
+    if avg<pass_mark:
+        return "fail"
+    elif avg>=distinction_mark:
+        return "distinction"
+    else:
+        return"pass"
+def letter_grade(avg):
+    if avg>=90:
+        return"A"
+    elif avg>=80:
+        return"B"
+    elif avg>=70:
+        return"C"
+    elif avg>=60:
+        return"D"
+    else:
+        return"F"
+def format_marks(m1,m2,m3):
+    return f"{m1},{m2},{m3}"
+print(average_of_three(85,90,78))
+print(classify_result(84.3))
+print(letter_grade(84.3))
+print(format_marks(85, 90, 78))
