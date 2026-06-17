878ad2194505af1356e431ff3c933d5c1fa4e619
shindeishwari642-cyber <shindeishwari642@gmail.com>
2026-06-17 11:30:54 +0530

    Create CP2.py

---
+++ students/ishwari-shinde/CP2.py
@@ -0,0 +1,9 @@
+
+def ask_text(prompt):
+    while True:
+        text = input(prompt).strip()
+        if text:
+            return text
+        print("Can't be empty — try again.")
+subject = ask_text("Subject name: ")
+print(subject)
