9eed57ee5bcb1cfb636e4f236ab79bac49ebdb40
Priya <priyapunde543@gmail.com>
2026-06-17 11:56:15 +0530

    Update cp2.py

--- students/priya-punde/cp2.py
+++ students/priya-punde/cp2.py
@@ -8,3 +8,17 @@ def ask_text(prompt):
         else:
             return"Can't be empty — try again."
 ask_text("a")
+#problem2
+def ask_mark(prompt):
+    while True:
+        try:
+            mark = int(input(prompt))
+
+            if 0 <= mark and mark <= 100:
+                return "mark"
+            else:
+                print("Mark between 0 and 100")
+
+        except ValueError:
+            print("enter a whole number.")
+ask_mark(46)
