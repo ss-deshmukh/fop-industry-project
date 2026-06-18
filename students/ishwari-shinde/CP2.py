be5aedcf35cbd115ddf6a5ad46abd7dfd1986d3e
shindeishwari642-cyber <shindeishwari642@gmail.com>
2026-06-18 11:26:50 +0530

    Update CP2.py

--- students/ishwari-shinde/CP2.py
+++ students/ishwari-shinde/CP2.py
@@ -1,4 +1,4 @@
-
+#problem 1 
 def ask_text(prompt):
     while True:
         text = input(prompt).strip()
@@ -8,7 +8,7 @@ def ask_text(prompt):
 subject = ask_text("Subject name: ")
 print(subject)
 
-
+#problem 2 
 def ask_mark(prompt):
     while True:
         text = input(prompt)
@@ -18,3 +18,45 @@ def ask_mark(prompt):
             return int(text)
         else:
             print("Mark must")
+
+#problem 3
+def collect_one_sub():
+    name = ask_text("Sub name: ")
+    m1 = ask_mark("  Mark 1 (0-100) ")
+    m2 = ask_mark("  Mark 2 (0-100) ")
+    m3 = ask_mark("  Mark 3 (0-100) ")
+    return (name, m1, m2, m3)
+
+    return("mark",85,90,78)
+
+#problem 4 
+def ask_yes_no(q):
+    while True:
+        a = input (q)
+        if a == ("y"):
+            return True
+        elif a == ("n"):
+            return False
+        else:
+            print('Please type "y" or "n".')
+result = ask_yes_no("Add another subject? (y/n): ")
+print(result)
+
+
+#problem 5
+
+def collect_all_sub():
+    sub = []
+
+    sub.append(collect_one_sub())
+
+    while len(sub) < MAX_SUB:
+        if ask_yes_no("Add another sub ? (y/n): "):
+            sub.append(collect_one_sub())
+        else:
+            break
+
+    if len(sub) == MAX_SUB:
+        print("Maximum 5 sub reached.")
+
+    return sub
