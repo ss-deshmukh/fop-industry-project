2f1c606b268de7c4788695fbcc7f2eb8f59fb1d7
akshadasadgir87-png <akshadasadgir87@gmail.com>
2026-06-18 11:36:02 +0530

    Update cp1.py

--- students/akshada-sadgir/cp1.py
+++ students/akshada-sadgir/cp1.py
@@ -1,4 +1,4 @@
-#problem1
+# CP1
 pass_mark=35
 distinction_mark=75
 def average_of_three(m1,m2,m3):
@@ -24,3 +24,37 @@ def letter_grade(avg):
         return"F"
 def format_marks(m1,m2,m3):
     return f"{m1},{m2},{m3}"
+#CP2
+def ask_prompt(p):
+ while true:
+    p=input("enter...")
+    if p=="":
+        print("cant be empty")
+    else:
+            return (p)
+ ask_prompt()
+def marks(i):
+    while True:
+        i=int(input("enter marks"))
+        if i.isdigit():
+            return (i)
+            print("enter correct marks ")
+            marks()
+def ask_yes_no(q):
+    while True:
+        a = input (q)
+        if a == ("y"):
+            return True
+        elif a == ("n"):
+            return False
+        else:
+            print('Please type "y" or "n".')
+result = ask_yes_no("Add another subject? (y/n): ")
+print(result)      
+def collect_one_subject():
+    name = ask_text("Subject name: ")
+    m1 = ask_mark("  Mark 1 (0-100): ")
+    m2 = ask_mark("  Mark 2 (0-100): ")
+    m3 = ask_mark("  Mark 3 (0-100): ")
+
+    return (name, m1, m2, m3)
